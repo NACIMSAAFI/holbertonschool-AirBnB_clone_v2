697bc7fd8d7f5beff2160f499bf9232905d82397
ChaimaBSlima <chaimabslima@outlook.fr>
2024-03-30 12:45:41 +0100

    return the main console with some modifications

--- console.py
+++ console.py
@@ -43,6 +43,27 @@ class HBNBCommand(cmd.Cmd):
         """Prints if isatty is false"""
         if not sys.__stdin__.isatty():
             print("(hbnb)")
+    
+        def _key_value_parser(self, args):
+            """creates a dictionary from a list of strings"""
+            new_dict = {}
+            for arg in args:
+                if "=" in arg:
+                    kvp = arg.split('=', 1)
+                    key = kvp[0]
+                    value = kvp[1]
+                    if value[0] == value[-1] == '"':
+                        value = shlex.split(value)[0].replace('_', ' ')
+                    else:
+                        try:
+                            value = int(value)
+                        except:
+                            try:
+                                value = float(value)
+                            except:
+                                continue
+                    new_dict[key] = value
+            return new_dict
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
