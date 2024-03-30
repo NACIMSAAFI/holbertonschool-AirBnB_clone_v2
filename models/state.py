81831f89f104fddf8c06102923899b87f464e969
ChaimaBSlima <chaimabslima@outlook.fr>
2024-03-30 10:44:19 +0100

    task 6 further modificfations

--- models/state.py
+++ models/state.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
-from os import getenv
 import models
 from models.city import City
+import shlex
 
-class State(BaseModel):
-    """ State class """
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="delete")
-    else:
-        @property
-        def cities(self):
-            """returns the list of City instances"""
-            instance_list = []
-            for i in models.storage.all(City).values():
-                if i.state_id == self.id:
-                    instance_list.append(i)
-            return instance_list
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
