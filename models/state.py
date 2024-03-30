e1c5aec56ac7c1b6c8bd83f786625439246f533d
ChaimaBSlima <chaimabslima@outlook.fr>
2024-03-30 10:14:01 +0100

    task 6 modifie state.py and db_storge.py

--- models/state.py
+++ models/state.py
@@ -11,7 +11,7 @@ class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    cities = relationship('City', backref='state', cascade='delete')
 
     @property
     def cities(self):
