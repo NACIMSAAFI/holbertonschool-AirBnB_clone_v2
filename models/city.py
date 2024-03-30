1ef9c37ed9c1f04fe6e63c4ff3f91427053dc911
ChaimaBSlima <chaimabslima@outlook.fr>
2024-03-30 09:57:45 +0100

    task 6 modifie city.py

--- models/city.py
+++ models/city.py
@@ -2,7 +2,9 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.place import Place
 
 
 class City(BaseModel):
@@ -10,3 +12,5 @@ class City(BaseModel):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
