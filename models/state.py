409f256b11ba9eff565884184103023ebb8ed1b5
ChaimaBSlima <chaimabslima@outlook.fr>
2024-03-30 10:03:39 +0100

    task 6 modifie state.py

--- models/state.py
+++ models/state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-
 
 class State(BaseModel):
     """ State class """
