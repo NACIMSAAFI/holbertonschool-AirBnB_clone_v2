193cf4685effed3bbcf92cc5f4bab566138feb97
NACIMSAAFI <7715@holbertonstudents.com>
2024-04-11 18:09:45 +0100

    Add 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -14,6 +14,3 @@ def hello_hbnb():
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
